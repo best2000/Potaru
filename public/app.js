3118f0b6a9d5d83f52f1bb9c258c3f457f09212c
best2000 <silentman@hotmail.co.th>
2020-11-27 02:47:42 +0700

    lol

--- public/app.js
+++ public/app.js
@@ -2,5 +2,6 @@ console.log(firebase)
 
 
 firebase.database().ref('/slist').once('value').then((snapshot) => {
-  console.log(snapshot.val()['1'])
+  console.log(snapshot.val()['4']['pic'])
+  document.getElementById("propic").src = snapshot.val()['4']['pic']
 });
