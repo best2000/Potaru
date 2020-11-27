8375275953f4d819cb0ec319bf5d5ad4ac351279
best2000 <silentman@hotmail.co.th>
2020-11-27 22:36:43 +0700

    lkol

--- public/app.js
+++ public/app.js
@@ -4,7 +4,7 @@ function slist_rand() {
   var btn = document.getElementById("rand_btn")
   btn.disabled = true
 
-  smax = 4 //max stars
+  smax = 5 //max stars
   let rn = Math.floor((Math.random() * smax) + 1)
   firebase.database().ref('/slist/'+rn.toString()).once('value').then((snapshot) => {
     let s = snapshot.val()
